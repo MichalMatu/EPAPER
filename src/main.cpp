92204c229a799acbc6fd266e85d3d2364ead9ef6
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-25 04:27:35 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,40 @@
 DEPG0213RWS800 display(PIN_DC, PIN_CS, PIN_BUSY); // 2.13" V2 - BWR - Red Tab
 void setup()
 {
-  DRAW(display)
-  {
-    display.fillCircle(75, 50, 50, BLACK);
-    display.fillCircle(75, 200, 50, RED);
-  }
+
+  // clear the display
+  display.clear();
+
+  // set the rotation of the display
+  display.setRotation(1);
 }
+
+// create variable to store refresh time
+unsigned long refreshTime = 0;
+int count = 0;
+
 void loop()
 {
+  // check if it's time to refresh the display
+  if (millis() - refreshTime >= 10000)
+  {
+    DRAW(display)
+    {
+      // set text size to 2
+      display.setTextSize(2);
+      display.setCursor(10, 10);
+      display.print("First line.");
+
+      display.setCursor(10, 40);
+      display.print("Second line.");
+
+      // display count
+      display.setCursor(10, 70);
+      display.print(count);
+      count++;
+    }
+
+    // update the refresh time
+    refreshTime = millis();
+  }
 }
