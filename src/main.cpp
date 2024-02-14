c214cfffc21099ecc28736227bf0867e4bfb8503
MichalMatu <michal.matuszewski1984@gmail.com>
2024-02-14 11:00:03 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,21 @@ DEPG0213RWS800 display(PIN_DC, PIN_CS, PIN_BUSY); // 2.13" V2 - BWR - Red Tab
 
 Adafruit_BME680 bme; // I2C
 // ****************************************************
+#include "Adafruit_SGP30.h"
 
+Adafruit_SGP30 sgp;
+
+/* return absolute humidity [mg/m^3] with approximation formula
+ * @param temperature [°C]
+ * @param humidity [%RH]
+ */
+uint32_t getAbsoluteHumidity(float temperature, float humidity)
+{
+  // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
+  const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
+  const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity);                                                                // [mg/m^3]
+  return absoluteHumidityScaled;
+}
 // ****************************************************
 
 SensirionI2CScd4x scd4x;
@@ -145,17 +159,18 @@ void setup()
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
+  Serial.println("SGP30 test");
 
-  // ****************************************************
-  // SGP30 sgp;
-  // if (!sgp.begin(Wire))
-  // {
-  //   Serial.println("Failed to initialize SGP30");
-  //   while (1)
-  //     ;
-  // }
-  // ****************************************************
-
+  if (!sgp.begin())
+  {
+    Serial.println("Sensor not found :(");
+    while (1)
+      ;
+  }
+  Serial.print("Found SGP30 serial #");
+  Serial.print(sgp.serialnumber[0], HEX);
+  Serial.print(sgp.serialnumber[1], HEX);
+  Serial.println(sgp.serialnumber[2], HEX);
   // ****************************************************
 
   // Set device as a Wi-Fi Station
@@ -257,6 +272,18 @@ void updateDisplay()
     display.setCursor(0, 60);
     display.print("MAC:");
     display.print(WiFi.macAddress());
+    // draw line
+    display.drawLine(0, 70, 250, 70, BLACK);
+    // display readings from SGP30
+    display.setCursor(0, 75);
+    display.print("TVOC:");
+    display.print(sgp.TVOC);
+
+    display.setCursor(60, 75);
+    display.print("eCO2:");
+    display.print(sgp.eCO2);
+
+    display.drawLine(0, 85, 250, 85, BLACK);
   }
 }
 // ****************************************************
@@ -350,7 +377,53 @@ void loop()
     Serial.println();
     // delay(2000);
     // ****************************************************
+    // If you have a temperature / humidity sensor, you can set the absolute humidity to enable the humditiy compensation for the air quality signals
+    // float temperature = 22.1; // [°C]
+    // float humidity = 45.2; // [%RH]
+    // sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
 
+    if (!sgp.IAQmeasure())
+    {
+      Serial.println("Measurement failed");
+      return;
+    }
+    Serial.print("TVOC ");
+    Serial.print(sgp.TVOC);
+    Serial.print(" ppb\t");
+    Serial.print("eCO2 ");
+    Serial.print(sgp.eCO2);
+    Serial.println(" ppm");
+
+    if (!sgp.IAQmeasureRaw())
+    {
+      Serial.println("Raw Measurement failed");
+      return;
+    }
+    Serial.print("Raw H2 ");
+    Serial.print(sgp.rawH2);
+    Serial.print(" \t");
+    Serial.print("Raw Ethanol ");
+    Serial.print(sgp.rawEthanol);
+    Serial.println("");
+
+    delay(1000);
+
+    counter++;
+    if (counter == 30)
+    {
+      counter = 0;
+
+      uint16_t TVOC_base, eCO2_base;
+      if (!sgp.getIAQBaseline(&eCO2_base, &TVOC_base))
+      {
+        Serial.println("Failed to get baseline readings");
+        return;
+      }
+      Serial.print("****Baseline values: eCO2: 0x");
+      Serial.print(eCO2_base, HEX);
+      Serial.print(" & TVOC: 0x");
+      Serial.println(TVOC_base, HEX);
+    }
     // ****************************************************
 
     scd41();
